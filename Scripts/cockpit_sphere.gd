3af3022056833778e03866a3b4e4b6214a4e9be3
echo storrs <echoatday@gmail.com>
2024-11-20 17:28:02 -0500

    More folder cleanup

--- Scripts/cockpit_sphere.gd
+++
@@ -1,7 +0,0 @@
-extends Node3D
-
-func _physics_process(delta: float) -> void:
-	transform.origin = owner.transform.origin
-	if !owner.state_rolling:
-		look_at(owner.camera.project_position((get_viewport().get_mouse_position()/9)+Vector2(get_viewport().size/2.26),10))
-	pass
--- Scripts/speedometer_control.gd
+++
@@ -1,9 +0,0 @@
-extends Label3D
-
-func _physics_process(delta: float) -> void:
-	var meter_speed = snapped(owner.velocity.length(),0.1)
-	if meter_speed > 99.6:
-		text = str("99.6")
-	elif Engine.get_physics_frames() % 10 == 0:
-			text = str(meter_speed).pad_decimals(1)
-	pass
