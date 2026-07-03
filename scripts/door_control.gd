636699b7f44384b49115418438f0fef79adc7bd2
echoatday <echoatday@gmail.com>
2026-07-03 19:12:51 -0400

    a lot of waste disposal mapping and some bugfixing to level loads

--- scripts/door_control.gd
+++ scripts/door_control.gd
@@ -4,7 +4,7 @@ extends Node
 # global_position
 var spawn_position
 @onready var spawn_room = 0
-@onready var door_rotation = self.global_rotation_degrees.y
+@onready var door_rotation = round(self.global_rotation_degrees.y)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _physics_process(delta: float) -> void:
