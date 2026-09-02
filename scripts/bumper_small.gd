0eee78162c184b7e09a470570731017e33096115
echoatday <echoatday@gmail.com>
2026-09-02 17:47:11 -0400

    a small bumper update and lots of forge works mapping

--- scripts/bumper_small.gd
+++ scripts/bumper_small.gd
@@ -8,6 +8,10 @@ func _physics_process(delta: float) -> void:
 
 func _on_area_3d_body_entered(body: Node3D) -> void:
 	body.velocity = -body.get_real_velocity()
+	if body.velocity.length() < 12:
+		body.velocity = body.velocity * 2
+	if body.is_on_floor():
+		body.velocity.y += 2
 
 
 func _on_area_3d_body_exited(body: Node3D) -> void:
