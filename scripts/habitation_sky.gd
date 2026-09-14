18e77c901212d09a3275bf1495ccb2ea6572505a
echoatday <echoatday@gmail.com>
2026-09-14 14:45:13 -0400

    huge habitation sky overhaul to make it look digital

---
+++ scripts/habitation_sky.gd
@@ -0,0 +1,24 @@
+extends Node3D
+
+@onready var sky_mesh = $FuncGodotMap/entity_0_worldspawn/entity_0_mesh_instance
+@onready var old_position = Globals.player.camera.global_position
+@onready var old_rotation = Globals.player.camera.global_rotation_degrees
+@onready var base_color = Color(2.0,2.0,2.1)
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	var new_position = Globals.player.camera.global_position
+	var pos_difference = old_position - new_position
+	var new_rotation = Globals.player.camera.global_rotation_degrees
+	var rot_difference = old_rotation - new_rotation
+	
+	if Engine.get_frames_drawn() % ceili(Engine.get_frames_per_second()/2) == 0:
+		sky_mesh.get_active_material(4).uv1_offset += Vector3(0.002,0,0.002)
+		sky_mesh.get_active_material(4).uv1_offset += Vector3(pos_difference.x + pos_difference.z,pos_difference.y,pos_difference.x + pos_difference.z) * -0.1
+		sky_mesh.get_active_material(4).uv1_offset += Vector3(rot_difference.y,-rot_difference.x,rot_difference.y) / 360
+	
+	old_position = new_position
+	old_rotation = new_rotation
+	
+	if Engine.get_frames_drawn() % ceili(Engine.get_frames_per_second()/randi_range(1,4)) == 0:
+		sky_mesh.get_active_material(4).albedo_color = base_color * randf_range(0.95,1.0)
